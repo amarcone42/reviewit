c881bf67e896d802112f5b1e22bb02afecf253d6
Antonio Marcone <40888021+amarcone42@users.noreply.github.com>
2024-01-17 20:30:22 +0100

    first commit

---
+++ src/main/java/it/amarcone/App.java
@@ -0,0 +1,13 @@
+package it.amarcone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, friend" );
+    }
+}
