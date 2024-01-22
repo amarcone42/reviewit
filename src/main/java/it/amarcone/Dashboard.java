ac082697c27893f5c1dd52130af24c797e5abecd
Antonio Marcone <40888021+amarcone42@users.noreply.github.com>
2024-01-23 00:01:52 +0100

    Updated GUI with better form layout Edited Dashboard.java with a spring layout for every form

--- src/main/java/it/amarcone/Dashboard.java
+++ src/main/java/it/amarcone/Dashboard.java
@@ -2,14 +2,12 @@ package it.amarcone;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.event.KeyEvent;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,21 +16,30 @@ import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
 import org.jdesktop.swingx.JXDatePicker;
-import org.jdesktop.swingx.painter.AbstractLayoutPainter.VerticalAlignment;
 
 public class Dashboard extends JFrame {
-    DatabaseManager database;
-    JPanel page;
-    JTextArea console;
-    
+    private final static int BORDER_EXT = 10;
+    private final static int GAP_BIG = 20;
+    private final static int GAP_MID = 10;
+    private final static int GAP_SMALL = 5;
+    private final static int FIELD_SIZE_L = 22;
+    private final static int FIELD_SIZE_M = 11;
+
+    private DatabaseManager database;
+    private JPanel page;
+    private JTextArea console;
+
+
     public Dashboard() {
         super("Reviewit");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
-        setSize(1000, 600);
+        setSize(900, 600);
         setResizable(true);
-        setMinimumSize(new Dimension(1000, 600));
+        setMinimumSize(new Dimension(800, 600));
 
         database = new DatabaseManager();
 
@@ -40,7 +47,7 @@ public class Dashboard extends JFrame {
         page.setLayout(new BoxLayout(page,BoxLayout.X_AXIS));
 
         //Fake console to show query output
-        console = new JTextArea(25,50);
+        console = new JTextArea(25,35);
         console.setEditable(false);
         JScrollPane scrollableTextArea = new JScrollPane(console);
 
@@ -65,11 +72,13 @@ public class Dashboard extends JFrame {
 
     public JPanel operazione04(){
         JPanel panel = new JPanel();
+        SpringLayout layout = new SpringLayout();
+        panel.setLayout(layout);
 
         JLabel label_titolo = new JLabel("Recuperare le opere viste da un utente tramite le recensioni");
 
         JLabel label_username = new JLabel("Username");
-        JTextField field_username = new JTextField(10);
+        JTextField field_username = new JTextField(FIELD_SIZE_L);
 
         JButton submit = new JButton("Cerca");
         submit.addActionListener(e -> {
@@ -87,24 +96,35 @@ public class Dashboard extends JFrame {
             console.setText("Opere viste da " + field_username.getText() + ":\n" + result);
         });
         panel.add(label_titolo);
+        layout.putConstraint(SpringLayout.WEST, label_titolo,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_titolo,BORDER_EXT, SpringLayout.NORTH, panel);
 
         panel.add(label_username);
         panel.add(field_username);
+        layout.putConstraint(SpringLayout.WEST, label_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_username,GAP_BIG,SpringLayout.SOUTH, label_titolo);
+        layout.putConstraint(SpringLayout.WEST, field_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_username,GAP_SMALL,SpringLayout.SOUTH, label_username);
 
         panel.add(submit);
+        layout.putConstraint(SpringLayout.WEST, submit,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, submit,GAP_MID,SpringLayout.SOUTH, field_username);
+
         return panel;
     }
 
     public JPanel operazione05(){
         JPanel panel = new JPanel();
+        SpringLayout layout = new SpringLayout();
+        panel.setLayout(layout);
 
         JLabel label_titolo = new JLabel("Recuperare da una lista tutte le opere che sono state già visualizzate e non");
 
         JLabel label_username = new JLabel("Username");
-        JTextField field_username = new JTextField(10);
+        JTextField field_username = new JTextField(FIELD_SIZE_L);
 
         JLabel label_listaId = new JLabel("Id lista");
-        JTextField field_listaId = new JTextField(10);
+        JTextField field_listaId = new JTextField(FIELD_SIZE_M);
 
         JRadioButton visto = new JRadioButton("Opere visualizzate");
         visto.setActionCommand("true");
@@ -133,30 +153,48 @@ public class Dashboard extends JFrame {
             console.setText("Opere della lista " + field_listaId.getText() +" di " + field_username.getText() + ":\n" + result);
         });
         panel.add(label_titolo);
+        layout.putConstraint(SpringLayout.WEST, label_titolo,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_titolo,BORDER_EXT, SpringLayout.NORTH, panel);
 
         panel.add(label_username);
         panel.add(field_username);
+        layout.putConstraint(SpringLayout.WEST, label_username,0,SpringLayout.WEST, label_titolo);
+        layout.putConstraint(SpringLayout.NORTH, label_username,GAP_BIG,SpringLayout.SOUTH, label_titolo);
+        layout.putConstraint(SpringLayout.WEST, field_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_username,GAP_SMALL,SpringLayout.SOUTH, label_username);
 
         panel.add(label_listaId);
         panel.add(field_listaId);
+        layout.putConstraint(SpringLayout.WEST, label_listaId,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_listaId,GAP_MID,SpringLayout.SOUTH, field_username);
+        layout.putConstraint(SpringLayout.WEST, field_listaId,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_listaId,GAP_SMALL,SpringLayout.SOUTH, label_listaId);
 
         panel.add(visto);
         panel.add(nonvisto);
+        layout.putConstraint(SpringLayout.WEST, visto,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, visto,GAP_MID,SpringLayout.SOUTH, field_listaId);
+        layout.putConstraint(SpringLayout.WEST, nonvisto,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, nonvisto,GAP_SMALL,SpringLayout.SOUTH, visto);
 
         panel.add(submit);
+        layout.putConstraint(SpringLayout.WEST, submit,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, submit,GAP_MID,SpringLayout.SOUTH, nonvisto);
         return panel;
     }
 
     public JPanel operazione06(){
         JPanel panel = new JPanel();
+        SpringLayout layout = new SpringLayout();
+        panel.setLayout(layout);
 
         JLabel label_titolo = new JLabel("Recuperare tutte le opere a cui ha partecipato un lavoratore");
 
         JLabel label_nome = new JLabel("Nome");
-        JTextField field_nome = new JTextField(10);
+        JTextField field_nome = new JTextField(FIELD_SIZE_L);
 
         JLabel label_cognome = new JLabel("Cognome");
-        JTextField field_cognome = new JTextField(10);
+        JTextField field_cognome = new JTextField(FIELD_SIZE_L);
 
         JButton submit = new JButton("Cerca");
         submit.addActionListener(e -> {
@@ -174,22 +212,39 @@ public class Dashboard extends JFrame {
             console.setText(result);
         });
         panel.add(label_titolo);
+        layout.putConstraint(SpringLayout.WEST, label_titolo,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_titolo,BORDER_EXT, SpringLayout.NORTH, panel);
 
         panel.add(label_nome);
         panel.add(field_nome);
+        layout.putConstraint(SpringLayout.WEST, label_nome,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_nome,GAP_BIG, SpringLayout.SOUTH, label_titolo);
+        layout.putConstraint(SpringLayout.WEST, field_nome,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_nome,GAP_SMALL, SpringLayout.SOUTH, label_nome);
 
         panel.add(label_cognome);
         panel.add(field_cognome);
+        layout.putConstraint(SpringLayout.WEST, label_cognome,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_cognome,GAP_MID, SpringLayout.SOUTH, field_nome);
+        layout.putConstraint(SpringLayout.WEST, field_cognome,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_cognome,GAP_SMALL, SpringLayout.SOUTH, label_cognome);
 
         panel.add(submit);
+        layout.putConstraint(SpringLayout.WEST, submit,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, submit,GAP_MID, SpringLayout.SOUTH, field_cognome);
+
         return panel;
     }
 
     public JPanel operazione07() {
         JPanel panel = new JPanel();
+        SpringLayout layout = new SpringLayout();
+        panel.setLayout(layout);
+
+        JLabel label_titolo = new JLabel("Recuperare le notizie pubblicate da un utente");
 
         JLabel label_username = new JLabel("Username");
-        JTextField field_username = new JTextField(10);
+        JTextField field_username = new JTextField(FIELD_SIZE_L);
 
         JButton submit = new JButton("Cerca");
         submit.addActionListener(e -> {
@@ -206,18 +261,33 @@ public class Dashboard extends JFrame {
             System.out.println(result);
             console.setText(result);
         });
+        panel.add(label_titolo);
+        layout.putConstraint(SpringLayout.WEST, label_titolo,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_titolo,BORDER_EXT, SpringLayout.NORTH, panel);
+
         panel.add(label_username);
         panel.add(field_username);
+        layout.putConstraint(SpringLayout.WEST, label_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_username,GAP_BIG,SpringLayout.SOUTH, label_titolo);
+        layout.putConstraint(SpringLayout.WEST, field_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_username,GAP_SMALL,SpringLayout.SOUTH, label_username);
 
         panel.add(submit);
+        layout.putConstraint(SpringLayout.WEST, submit,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, submit,GAP_MID,SpringLayout.SOUTH, field_username);
+
         return panel;
     }
 
     public JPanel operazione08() {
         JPanel panel = new JPanel();
+        SpringLayout layout = new SpringLayout();
+        panel.setLayout(layout);
+
+        JLabel label_titolo = new JLabel("Opere viste in un range temporale");
 
         JLabel label_username = new JLabel("Username");
-        JTextField field_username = new JTextField(10);
+        JTextField field_username = new JTextField(FIELD_SIZE_L);
 
         JLabel label_dataMin = new JLabel("Data inizio");
         JXDatePicker pickerDataMin = new JXDatePicker ();
@@ -248,24 +318,47 @@ public class Dashboard extends JFrame {
             System.out.println(result);
             console.setText(result);
         });
+        panel.add(label_titolo);
+        layout.putConstraint(SpringLayout.WEST, label_titolo,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_titolo,BORDER_EXT, SpringLayout.NORTH, panel);
+
         panel.add(label_username);
         panel.add(field_username);
+        layout.putConstraint(SpringLayout.WEST, label_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_username,GAP_BIG,SpringLayout.SOUTH, label_titolo);
+        layout.putConstraint(SpringLayout.WEST, field_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_username,GAP_SMALL,SpringLayout.SOUTH, label_username);
 
         panel.add(label_dataMin);
         panel.add(pickerDataMin);
+        layout.putConstraint(SpringLayout.WEST, label_dataMin,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_dataMin,GAP_MID,SpringLayout.SOUTH, field_username);
+        layout.putConstraint(SpringLayout.WEST, pickerDataMin,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, pickerDataMin,GAP_SMALL,SpringLayout.SOUTH, label_dataMin);
 
         panel.add(label_dataMax);
         panel.add(pickerDataMax);
+        layout.putConstraint(SpringLayout.WEST, label_dataMax,60,SpringLayout.EAST, label_dataMin);
+        layout.putConstraint(SpringLayout.NORTH, label_dataMax,0,SpringLayout.NORTH, label_dataMin);
+        layout.putConstraint(SpringLayout.WEST, pickerDataMax,0,SpringLayout.WEST, label_dataMax);
+        layout.putConstraint(SpringLayout.NORTH, pickerDataMax,GAP_SMALL,SpringLayout.SOUTH, label_dataMax);
 
         panel.add(submit);
+        layout.putConstraint(SpringLayout.WEST, submit,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, submit,GAP_MID,SpringLayout.SOUTH, pickerDataMin);
+
         return panel;
     }
 
     public JPanel operazione09() {
         JPanel panel = new JPanel();
+        SpringLayout layout = new SpringLayout();
+        panel.setLayout(layout);
+
+        JLabel label_titolo = new JLabel("Numero opere viste in un range temporale");
 
         JLabel label_username = new JLabel("Username");
-        JTextField field_username = new JTextField(10);
+        JTextField field_username = new JTextField(FIELD_SIZE_L);
 
         JLabel label_dataMin = new JLabel("Data inizio");
         JXDatePicker pickerDataMin = new JXDatePicker ();
@@ -301,24 +394,47 @@ public class Dashboard extends JFrame {
             System.out.println(result);
             console.setText(result);
         });
+        panel.add(label_titolo);
+        layout.putConstraint(SpringLayout.WEST, label_titolo,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_titolo,BORDER_EXT, SpringLayout.NORTH, panel);
+
         panel.add(label_username);
         panel.add(field_username);
+        layout.putConstraint(SpringLayout.WEST, label_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_username,GAP_BIG,SpringLayout.SOUTH, label_titolo);
+        layout.putConstraint(SpringLayout.WEST, field_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_username,GAP_SMALL,SpringLayout.SOUTH, label_username);
 
         panel.add(label_dataMin);
         panel.add(pickerDataMin);
+        layout.putConstraint(SpringLayout.WEST, label_dataMin,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_dataMin,GAP_MID,SpringLayout.SOUTH, field_username);
+        layout.putConstraint(SpringLayout.WEST, pickerDataMin,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, pickerDataMin,GAP_SMALL,SpringLayout.SOUTH, label_dataMin);
 
         panel.add(label_dataMax);
         panel.add(pickerDataMax);
+        layout.putConstraint(SpringLayout.WEST, label_dataMax,60,SpringLayout.EAST, label_dataMin);
+        layout.putConstraint(SpringLayout.NORTH, label_dataMax,0,SpringLayout.NORTH, label_dataMin);
+        layout.putConstraint(SpringLayout.WEST, pickerDataMax,0,SpringLayout.WEST, label_dataMax);
+        layout.putConstraint(SpringLayout.NORTH, pickerDataMax,GAP_SMALL,SpringLayout.SOUTH, label_dataMax);
 
         panel.add(submit);
+        layout.putConstraint(SpringLayout.WEST, submit,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, submit,GAP_MID,SpringLayout.SOUTH, pickerDataMin);
+
         return panel;
     }
 
     public JPanel operazione11() {
         JPanel panel = new JPanel();
+        SpringLayout layout = new SpringLayout();
+        panel.setLayout(layout);
+
+        JLabel label_titolo = new JLabel("Conteggio follower");
 
         JLabel label_username = new JLabel("Username");
-        JTextField field_username = new JTextField(10);
+        JTextField field_username = new JTextField(FIELD_SIZE_L);
 
         JButton submit = new JButton("Calcola");
         submit.addActionListener(e -> {
@@ -339,9 +455,21 @@ public class Dashboard extends JFrame {
             System.out.println(result);
             console.setText(result);
         });
+        panel.add(label_titolo);
+        layout.putConstraint(SpringLayout.WEST, label_titolo,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_titolo,BORDER_EXT, SpringLayout.NORTH, panel);
+
         panel.add(label_username);
         panel.add(field_username);
+        layout.putConstraint(SpringLayout.WEST, label_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, label_username,GAP_BIG,SpringLayout.SOUTH, label_titolo);
+        layout.putConstraint(SpringLayout.WEST, field_username,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, field_username,GAP_SMALL,SpringLayout.SOUTH, label_username);
+
         panel.add(submit);
+        layout.putConstraint(SpringLayout.WEST, submit,BORDER_EXT,SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, submit,GAP_MID,SpringLayout.SOUTH, field_username);
+
         return panel;
     }
 }
