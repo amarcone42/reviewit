fe5589077ac102da18b2db85cb511b383f756bbd
Antonio Marcone <40888021+amarcone42@users.noreply.github.com>
2024-01-23 17:19:00 +0100

    Added support for FlatLaf to style the GUI Edited pom.xml to add FlatLaf dependency. Edited App.java to use the FlatLaf look and feel  manager. Added FlatMacDarkLaf.properties and FlatMacLightLaf.properties to customize the theme style.

--- src/main/java/it/amarcone/App.java
+++ src/main/java/it/amarcone/App.java
@@ -1,20 +1,29 @@
 package it.amarcone;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.util.SystemInfo;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
 
         SwingUtilities.invokeLater( new Runnable() {
             public void run() {
-                try {
-                    UIManager.setLookAndFeel(
-                        UIManager.getSystemLookAndFeelClassName());
-                } catch (Exception useDefault) {}
+                FlatLaf.registerCustomDefaultsSource( "it.amarcone.themes" );
+                FlatMacDarkLaf.setup();
+
+                if( SystemInfo.isLinux ) {
+                    // enable custom window decorations
+                    JFrame.setDefaultLookAndFeelDecorated( true );
+                    JDialog.setDefaultLookAndFeelDecorated( true );
+                }
                 new Dashboard();
             }
         });
